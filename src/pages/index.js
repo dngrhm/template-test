ffada5e0abd435a8ceb32c83ca181cbb75709b6e
Daniel Graham <dfgraham@gmail.com>
2020-07-21 12:38:00 -0400

    feat: working import from github component lib package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import {Button} from "@dngrhm/test-component-lib"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -14,6 +14,10 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Button
+        text="Hello"
+        onClick={() => console.log("clicked!")}
+      />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
